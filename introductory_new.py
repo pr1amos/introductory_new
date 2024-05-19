0a6f7cd909308c0f5c256668500b25fba52e7f40
pr1amos <157016419+pr1amos@users.noreply.github.com>
2024-05-19 19:58:09 +0300

    Add files via upload

---
+++ introductory_new.py
@@ -0,0 +1,24 @@
+name = 66
+print(type(str(66)))
+print(len('66'))
+
+
+a = 8
+b = 1
+sum = a + b
+difference = a - b
+print(str(sum))
+print(str(difference))
+
+print(9 - 4 + 17)
+print('2' + '2')
+
+a = 'вторник'
+b = 'понедельник'
+print(b + ', ' + a)
+
+a = float(1)
+b = float(2)
+c = float(3)
+x = (a + b + c) / 3
+print(str(x))
